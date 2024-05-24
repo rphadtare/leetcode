55d292193c0059829cbd54bf351d86fca3698c04
rphadtare <rohitphadtare39@gmail.com>
2024-05-24 19:18:38 +0200

    2024-05-24 Committing solutions for Third Maximum Number problem

--- phase1/src/main/python/leetcode.arr.solutions/ArraySolutions.py
+++ phase1/src/main/python/leetcode.arr.solutions/ArraySolutions.py
@@ -80,3 +80,63 @@ class Solution:
                 tmp_max_consecutive_one_values = 0
 
         return max_consecutive_one_values
+
+
+##
+#
+# /***
+#      *
+#      * Input: nums = [3,2,1]
+#      * Output: 1
+#      * Explanation:
+#      * The first distinct maximum is 3.
+#      * The second distinct maximum is 2.
+#      * The third distinct maximum is 1.
+#      *
+#      *
+#      * Input: nums = [1,2]
+#      * Output: 2
+#      * Explanation:
+#      * The first distinct maximum is 2.
+#      * The second distinct maximum is 1.
+#      * The third distinct maximum does not exist, so the maximum (2) is returned instead.
+#      *
+#      *
+#      * Input: nums = [2,2,3,1]
+#      * Output: 1
+#      * Explanation:
+#      * The first distinct maximum is 3.
+#      * The second distinct maximum is 2 (both 2's are counted together since they have the same value).
+#      * The third distinct maximum is 1.
+#      *
+# #
+
+
+def thirdMax(self, nums: list[int]) -> int:
+    max_val = second_max_val = third_max_val = None
+    len_of_nums = len(nums)
+    for num in nums:
+
+        if max_val == num or second_max_val == num or third_max_val == num:
+            continue
+
+        if max_val is None or max_val < num:
+            if max_val is None:
+                max_val = num
+            else:
+                third_max_val = second_max_val
+                second_max_val = max_val
+                max_val = num
+        elif second_max_val is None or second_max_val < num:
+            if second_max_val is None:
+                second_max_val = num
+            else:
+                third_max_val = second_max_val
+                second_max_val = num
+        elif third_max_val is None or third_max_val < num:
+            third_max_val = num
+
+    if third_max_val is None:
+        return max_val
+    else:
+        return third_max_val
